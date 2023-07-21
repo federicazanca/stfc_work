6c6ea3c28f8effb9f49bfbb2662f28c402316838
federica <federicazanca8@gmail.com>
2023-07-21 10:57:39 +0100

    sort lists in converg analysis

--- convergence_analysis.py
+++ convergence_analysis.py
@@ -19,12 +19,14 @@ folder = sys.argv[1]
 out = folder.split("/")[-2]
 results = open(out+"_results.csv", "w")
 results.write(out+",energy\n")
+x_list = []
+y_list = []
 for file in os.listdir(folder):
     if file.endswith("log"):
         print(file)
         x = file.split("_")[1].split(".")[0]
         print(x)
-        results.write(x)
+        x_list.append(x)
         open_file = open(folder+file, 'r')
         Lines = open_file.readlines()
         if converged(folder+file) == True:
@@ -33,7 +35,12 @@ for file in os.listdir(folder):
                 if "!    total energy              =" in line:
                     y = line.split()[-2]
                     print(y)
-                    results.write(','+y+"\n")
+                    y_list.append(y)
+                    #results.write(','+y+"\n")
         else:
             print("not converged")
+x_list.sort()
+y_list.sort()
+for i in range(len(x_list)):
+    results.write(x_list[i]+","+y_list[i]+"\n")
 results.close()

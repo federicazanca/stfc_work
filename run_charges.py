624940c664f2538a72035805131bacb3bd0b6005
federica <federicazanca8@gmail.com>
2023-09-04 15:11:42 +0100

    continue run charges file

--- run_charges.py
+++ run_charges.py
@@ -31,28 +31,36 @@ if len(sys.argv) < 2:
 
 
 folder = sys.argv[1]
-input_files = "/work4/scd/scarf1228/uio/"
+#input_files = "/work4/scd/scarf1228/uio/"
+input_files = "/home/federica/"
 for root, dirs, files in os.walk(folder):
         for file in files:
               #find the scf files (they should be 2 in each folder) and get the needed info: scf folder and MOF name
               if file.endswith("scf.in"):
                     mofname = file[:-7]
-                    for line in open(file,"r"):
+                    print(mofname)
+                    for line in open(folder+file,"r"):
                          #scf folder
                          if "outdir" in line:
-                            scf_dir = line.split()[2]
+                            scf_dir = (line.split()[2]).split("/")[1]
                             print(scf_dir)
                     #create folder for bader charges for each mof
-                    os.makedirs(os.path+"bader"+mofname)
-                    target = os.path+"bader"+mofname
-                    shutil.copyfile(input_files+"pp*  " + target)
+                    os.makedirs(folder+"bader"+mofname)
+                    target = folder+"bader"+mofname
+                    shutil.copyfile(input_files+"pp.in",  target+"/pp.in")
+                    shutil.copyfile(input_files+"pp.slurm",  target+"/pp.slurm")
                     inp = target + "/pp.in"
+                    slurm = target + "/pp.slurm"
                     #modify pp files for specific mof inside the bader folder
                     for parameter in open(inp):
                         if "outdir = '../uio-scf2'" in parameter:
-                             replace(inp, "'../uio-scf2'", "'../"+scf_dir+"'")
+                             replace(inp, "'../uio-scf2'", "'../"+scf_dir)
                         if "fileout = 'uio-CH3_2_Pdc.cube'" in parameter:
                              replace(inp, "uio-CH3_2_Pdc.cube", mofname+".cube")
+                    for i in open(slurm):
+                         if "uio-CH3_Pdc-charge.log" in i:
+                              replace(slurm, "uio-CH3_Pdc", mofname)
+                    os.system(f"cd {target}; sbatch pp.slurm")
 
                              
                              

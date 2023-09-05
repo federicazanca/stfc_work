1b439cf89cba1e2f0fcff76a51b13e05ed1c6b31
federica <federicazanca8@gmail.com>
2023-09-05 11:32:36 +0100

    scf correction file

--- scf_correction.py
+++ scf_correction.py
@@ -26,29 +26,38 @@ def replace(file_path, pattern, subst):
     move(abs_path, file_path)
 
 if len(sys.argv) < 2:
-    print("ERROR: not enough arguments.  Usage: 'python crun....py  source_directory' to only get the results, 'python convergence_analysis.py  source_directory outputfile' to get the plot saved with the name outputfile")
+    print("ERROR: not enough arguments.  Usage: 'python run....py  source_directory' to only get the results, 'python convergence_analysis.py  source_directory outputfile' to get the plot saved with the name outputfile")
     exit()
 
 
 folder = sys.argv[1]
 
 for root, dirs, files in os.walk(folder):
+        a=11
         for file in files:
-              #find the scf files (they should be 2 in each folder) and get the needed info: scf folder and MOF name
-              if file.endswith("scf.in"):
-                    mofname = file[:-7]
-                    print(mofname)
-                    correct = True
-                    # Construct the full path to the file using os.path.join
-                    full_path = os.path.join(root, file)
-                    for line in open(full_path, "r"):
-                         #scf folder
-                         if "outdir" in line:
-                            scf_dir = (line.split()[2]).split("/")[1]
-                            print(scf_dir)
-                         if "disk_io          = 'none'" in line:
-                            correct = False
-                            print(mofname+" didn't run the complete scf")
-                            break
-                    if correct == False:
-                         
+            #find the scf files (they should be 2 in each folder) and get the needed info: scf folder and MOF name
+            if file.endswith("scf.in"):
+                mofname = file[:-7]
+                outp = file[:-2]+"log"
+                print(mofname)
+                correct = True
+                # Construct the full path to the file using os.path.join
+                full_path = os.path.join(root, file)
+                for line in open(full_path, "r"):
+                    if "disk_io          = 'none'" in line:
+                        correct = False
+                        print(mofname+" didn't run the complete scf")
+                        break
+                if correct == False:
+                    replace(full_path, "uio-scf", "uio-scf"+str(a)) 
+                    replace(full_path,"none", "low")     
+                
+                    print(f"cd {root}; sbatch qe-run.slurm {file} {outp}")
+                    os.system(f"cd {root}; sbatch qe-run.slurm {file} {outp}")
+                a += 1 
+
+
+                             
+                             
+
+ 

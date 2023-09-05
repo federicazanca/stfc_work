cfdd1bbaeb60affdc8caab38f765bed871a3676b
federica <federicazanca8@gmail.com>
2023-09-05 10:46:20 +0100

    corrections to paths

--- scf_correction.py
+++ scf_correction.py
@@ -31,15 +31,14 @@ if len(sys.argv) < 2:
 
 
 folder = sys.argv[1]
-input_files = "/work4/scd/scarf1228/uio/"
-#input_files = "/home/federica/"
+
 for root, dirs, files in os.walk(folder):
         for file in files:
               #find the scf files (they should be 2 in each folder) and get the needed info: scf folder and MOF name
               if file.endswith("scf.in"):
                     mofname = file[:-7]
                     print(mofname)
-                    ready = True
+                    correct = True
                     # Construct the full path to the file using os.path.join
                     full_path = os.path.join(root, file)
                     for line in open(full_path, "r"):
@@ -48,36 +47,8 @@ for root, dirs, files in os.walk(folder):
                             scf_dir = (line.split()[2]).split("/")[1]
                             print(scf_dir)
                          if "disk_io          = 'none'" in line:
-                            ready = False
+                            correct = False
                             print(mofname+" didn't run the complete scf")
                             break
-                    if ready == True:
-                        #create folder for bader charges for each mof
-                        print("making directory for "+mofname)
-                        target = os.path.join(root, "bader", mofname)
-                        print(target)
-                        if os.path.isdir(target):
-                            print("directory already exists")
-                        else:
-                            os.makedirs(target)
-                            shutil.copyfile(input_files+"pp.in",  target+"/pp.in")
-                            shutil.copyfile(input_files+"pp.slurm",  target+"/pp.slurm")
-                            inp = target + "/pp.in"
-                            slurm = target + "/pp.slurm"
-                            #modify pp files for specific mof inside the bader folder
-                            for parameter in open(inp):
-                                if "outdir = '../uio-scf2'" in parameter:
-                                    replace(inp, "'../uio-scf2'", "'../"+scf_dir)
-                                if "fileout = 'uio-CH3_2_Pdc.cube'" in parameter:
-                                    replace(inp, "uio-CH3_2_Pdc.cube", mofname+".cube")
-                            for i in open(slurm):
-                                if "uio-CH3_Pdc-charge.log" in i:
-                                    replace(slurm, "uio-CH3_Pdc", mofname)
-                                if "UiO-66Zr-Pd-8.cube" in i:
-                                    replace(slurm, "UiO-66Zr-Pd-8.cube", mofname+".cube")
-                            os.system(f"cd {target}; sbatch pp.slurm")
-
-                             
-                             
-
-
+                    if correct == False:
+                         
